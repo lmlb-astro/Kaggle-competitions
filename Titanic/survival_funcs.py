bb6c6bc85f30122491668b663d028083f7a03d26
lmlb-astro <136018556+lmlb-astro@users.noreply.github.com>
2024-03-31 08:54:19 -0700

    small changes to function, accuracy still 78%

--- Titanic/survival_funcs.py
+++ Titanic/survival_funcs.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 ####################
 
-
+## transform the column names with non-numerical categories (i.e. enums)
+## to columns with numerical values based on the information provided in a hashmap
 def trans_cols(df, col_name, new_name, trans_dict):
     for key in trans_dict.keys():
         df.loc[df[col_name] == key, new_name] = trans_dict[key]
     return df
 
 
+## get features for the train/fitting of the model
 def get_features(df, col_names):
     ## loop over the column names and append the results
     feat_list = []
@@ -18,3 +20,15 @@ def get_features(df, col_names):
 
     ## return the list as a transposed array
     return np.array(feat_list).T
+
+## Get the survival rate for the groups of a given category
+def get_group_probabilities(df, category):
+    ## perform count and survival sum for each group
+    df_count = df.groupby(category)["Survived"].count()
+    df_sum = df.groupby(category)["Survived"].sum()
+
+    ## calculate the survival rate
+    surv_rate = df_sum/df_count
+
+    ## return data frame with the count in each group and their survival rate
+    return pd.DataFrame(data = {"survival rate": surv_rate, "people in group": df_count})
