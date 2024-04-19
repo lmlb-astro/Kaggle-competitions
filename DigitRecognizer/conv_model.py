bd4c72141b4fc8d6caa03d7273e122a250996bb5
lmlb-astro <136018556+lmlb-astro@users.noreply.github.com>
2024-04-18 18:11:13 -0700

    larger residual model

--- DigitRecognizer/conv_model.py
+++ DigitRecognizer/conv_model.py
@@ -10,7 +10,8 @@ import ResBlock as resbl
 # 1) ConvBlock(): A block with two convolutional layers connected through batch normalization
 # 2) DB(): A downsample block which performs max pooling followed by a ConvBlock()
 # 3) ConvModel(): A convolutional network for MNIST digit classification, consisting of a ConvBlock(), a DB(), a max_pool and a dense fully connected layer.
-# 4) ResModel(): A residual convolutional network for MNIST digit classification, consisting of a ResBlock(), a DB(), a max_pool and a dense fully connected layer.
+# 4) ResModel(): A residual convolutional network for MNIST digit classification, consisting of a ResBlock(), a max_pool and a dense fully connected layer.
+# 5) ResModelLarge(): A largeresidual convolutional network for MNIST digit classification, consisting of a 3 x ResBlock() before each max pooling, a max_pool and a dense fully connected layer.
 #####################################################
 
 
@@ -122,7 +123,7 @@ class ConvModel(tf.keras.Model):
         return self.dense_final(x)
 
 
-## class for the convolutional model
+## class for the residual model
 class ResModel(tf.keras.Model):
     ## initialize the ConvModel
     def __init__(self, filters = [16, 32], drop_rate = 0.2, kernel_size = (3, 3)):
@@ -182,6 +183,73 @@ class ResModel(tf.keras.Model):
         return self.dense_final(x)
 
 
+## class for the residual large model
+class ResModelLarge(tf.keras.Model):
+    ## initialize the ConvModel
+    def __init__(self, filters = [16, 32], drop_rate = 0.2, kernel_size = (3, 3)):
+        ## initialize the parent class
+        super().__init__()
+
+        ## add the input layer
+        self.input_layer = tf.keras.layers.InputLayer(input_shape = (28, 28, 1))
+        
+        ## The first convolutional, the residual and max pool blocks
+        self.conv1 = tf.keras.layers.Conv2D(filters = filters[0], kernel_size = kernel_size, strides = (1, 1), padding = 'SAME')
+        self.batch_norm1 = tf.keras.layers.BatchNormalization()
+        self.resblock1_1 = resbl.ResBlock(filters = filters[0], kernel_size = kernel_size)
+        self.resblock1_2 = resbl.ResBlock(filters = filters[0], kernel_size = kernel_size)
+        self.resblock1_3 = resbl.ResBlock(filters = filters[0], kernel_size = kernel_size)
+        self.mp2d1 = tf.keras.layers.MaxPool2D()
+        self.resblock2_1 = resbl.ResBlock(filters = filters[1], kernel_size = kernel_size)
+        self.resblock2_2 = resbl.ResBlock(filters = filters[1], kernel_size = kernel_size)
+        self.resblock2_3 = resbl.ResBlock(filters = filters[1], kernel_size = kernel_size)
+        self.mp2d2 = tf.keras.layers.MaxPool2D()
+
+        ## flatten
+        self.flatten = Flatten(dtype='float32')
+
+        ## dense layers
+        self.d1 = tf.keras.layers.Dense(256, activation="relu")
+
+        ## The dropout layer with custom rate
+        self.drop1 = tf.keras.layers.Dropout(drop_rate)
+        self.drop2 = tf.keras.layers.Dropout(drop_rate)
+        
+        self.dense_final = tf.keras.layers.Dense(10, activation="softmax")
+        
+    
+    ## call the model
+    def call(self, inputs):
+
+        ## get the inputs
+        inputs = self.input_layer(inputs)
+
+        ## first convolutional layer and batch normalization
+        x = self.conv1(inputs)
+        x = self.batch_norm1(x)
+        
+        ## convolutional and max pooling 
+        x = self.resblock1_1(x) ## first group of layers
+        x = self.resblock1_2(x)
+        x = self.resblock1_3(x)
+        x = self.mp2d1(x)
+        
+        x = self.resblock2_1(x) ## second group of layers
+        x = self.resblock2_2(x)
+        x = self.resblock2_3(x)
+        x = self.mp2d2(x)
+        x = self.drop1(x)
+        
+        ## flatten
+        x = self.flatten(x)
+
+        ## dense layers
+        x = self.d1(x)
+
+        ## perform dropout
+        x = self.drop2(x)
+        
+        return self.dense_final(x)
 
 
 
